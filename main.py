c4a4433fa45390e5c184e101a1d9a69a7b0afd42
logmiranowski <logmiranowski@gmail.com>
2026-02-15 21:42:54 -0600

    List tickers, make sure 'charts' exists, download data.

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yfinance
 
+# List of tickers
+tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'META']
+
+#make sure 'charts' folder exists
+if not os.path.exists('charts'):
+    os.makedirs('charts')
+
+# Loop through each ticker
+for ticker in tickers:
+    # Dowload last 10 trading days of data
+    data = yfinance.download(ticker, period='10d')
+    closing_prices = data['Close'].values
+    days = np.arange(1, len(closing_prices) + 1)
