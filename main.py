fb6001f8df47007e0eab279d75a471960e196e32
logmiranowski <logmiranowski@gmail.com>
2026-02-15 22:25:07 -0600

    test push

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #Logan Miranowski
-#INF601 - Spring 2026
+#INF601 - Advanced Programming in Python
 #Mini-Project 1
 
 import os 
 import matplotlib.pyplot as plt
 import numpy as np
-import yfinance
+import yfinance as yf
 
 # List of tickers
 tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'META']
@@ -17,14 +17,15 @@ if not os.path.exists('charts'):
 # Loop through each ticker
 for ticker in tickers:
     # Dowload last 10 trading days of data
-    data = yfinance.download(ticker, period='10d')
-    closing_prices = data['Close'].values
+    data = yf.download(ticker, period='10d')
+    closing_prices = data['Close'].values #Convert to numpy array for easier plotting
     days = np.arange(1, len(closing_prices) + 1)
 
     # Plot graph
     plt.figure(figsize=(8,5))
     plt.plot(days, closing_prices, marker='o', linestyle='-', color='green')
     plt.title(f"{ticker} Closing Prices for Last 10 Trading Days")
-    plt.xlabel("Day")
+    plt.xlabel("Trading Days Ago")
     plt.ylabel("Closing Price")
     plt.grid(True)
+
